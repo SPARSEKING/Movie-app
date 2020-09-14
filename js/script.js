4e4531e45b7bb477bff0b3879e4ccd8606941117
Kirill <gavrilchikkirill6@gmail.com>
2020-09-14 12:57:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let answers = [];
+
+answers[0] = prompt("Как вас зовут?", "");
+answers[1] = prompt("В каком городе живете?", "");
+answers[2] = prompt("Скольк вам лет?", "");
+
+document.write(answers)
